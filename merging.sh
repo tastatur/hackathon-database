6d599df090974c07886b151983b700163fba899e
tastatur <denis.repkov.dui@gmail.com>
2016-09-03 18:59:47 +0200

    Todesfälle

--- merging.sh
+++ merging.sh
@@ -1,1 +1,1 @@
-jq -s '.[0] * .[1] * .[2] * .[3]' pm10.parsed.json krank.parsed.json pm25.parsed.json no2.parsed.json > merged.json
+jq -s '.[0] * .[1] * .[2] * .[3] * .[4]' pm10.parsed.json krank.parsed.json pm25.parsed.json no2.parsed.json tot.parsed.json > merged.json
