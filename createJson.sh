0f38c539c17e9959b61ecd26d862cfd24e6d79b4
tastatur <denis.repkov.dui@gmail.com>
2016-09-03 15:11:35 +0200

    Parsed json

---
+++ createJson.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+INPUT=$1
+YEAR=$2
+MEASUREMENT=$3
+#Parse city->ID mapping
+cat ${INPUT} | jq ".stadt" | grep -v "[\{\}]" > cities.tmp
+#Parse id->value (no2 etc
+cat ${INPUT} | jq ".jahr_${YEAR}" | grep -v "[\{\}]" > values.tmp
+
+##Calculate averages
+#cat values.tmp | grep -v "null" | while read i; do
+#  CODE=$(echo "$i" | awk -F ":" '{print $1}')
+#  VALUE=$(echo "$i" | awk -F ":" '{print $2}')
+
+
+#done
+
+echo "{"
+cat values.tmp | grep -v "null" | while read i; do
+  CODE=$(echo "$i" | awk -F ":" '{print $1}')
+  VALUE=$(echo "$i" | awk -F ":" '{print $2}')
+  CITY=$(cat cities.tmp | grep "$CODE" |  awk -F ":" '{print $2}')
+  echo "${CITY}:{ ${MEASUREMENT}: $VALUE }"
+done
+echo "}"
+
+rm cities.tmp
+rm values.tmp
